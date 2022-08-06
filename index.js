0003af60a8d71b2677cd0f3f3e393e791940e26f
Ansh Malhotra <67304812+Ansh757@users.noreply.github.com>
2022-08-06 13:17:19 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,87 @@
+/*
+* Application state
+*/
+    const state = {
+        items: [],
+        count: 0,
+        isDone: false
+    }
+
+    function drawLine(box) {
+        let connector = box.children[0];
+        let item = document.querySelector('.__item');
+        let text = document.querySelector('.__item .text');
+
+        let distanceBetweenItemAndLine = (item.offsetWidth / 4) - text.offsetWidth;
+
+        if (box.classList.contains('__left')) {
+        connector.style.left = text.offsetWidth  + 380 + 'px';
+        } else {
+        connector.style.right = text.offsetWidth + 380 + 'px';
+        }
+
+        // Adds line after transition time specified in CSS is done
+        setTimeout(function () { connector.style.width = distanceBetweenItemAndLine + 100 + 'px'; }, 600);
+    }
+
+    function shouldItemAppear(item) {
+        let x = item.getBoundingClientRect().top;
+        if (x <= (window.innerHeight - item.offsetHeight)) {
+
+        //Is the item a goal?
+        //If so apply a different transformation
+        if (item.classList.contains('__goals')) {
+
+            //Bring in the coin first
+            item.children[1].style.opacity = '1'
+            //Then the rest
+            setTimeout(() => { item.children[2].style.opacity = '1'; }, 500);
+
+        } 
+        
+        else {
+
+            item.style.transform = 'translateX(0)'
+            drawLine(item)
+
+        }
+
+        //Count items then call isDone
+        state.count++;
+        state.count == state.items.length ? state.isDone = true : null;
+        }
+    }
+
+    function isScrolling() {
+        //Stop firing
+        if (state.isDone == true) {
+        window.removeEventListener('scroll', isScrolling);
+        window.cancelAnimationFrame(isScrolling);
+        return;
+        }
+
+        shouldItemAppear(state.items[state.count]);
+        window.requestAnimationFrame(isScrolling);
+    }
+
+    function getAllItems() {
+        var scrollY = window.scrollY + window.innerHeight;
+        var items = document.querySelectorAll('#roadmap .__item');
+
+        for (var x = 0; x < items.length; x++) {
+        state.items.push(items[x])
+        }
+
+    }
+
+    function checkResolution() {
+        if (window.innerWidth > 850) {
+        getAllItems();
+        window.addEventListener('scroll', isScrolling);
+        } else {
+        drawLinesMobile();
+        }
+    }
+
+    window.addEventListener('load', checkResolution);
+    window.requestAnimationFrame(isScrolling);
